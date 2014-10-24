a3302268835984f0a8bc8234a5fd5a682caad9bf
fy24h <fy24h.huang@gmail.com>
2014-10-24 15:54:07 +0800

    init

---
+++ src/MvcApplication3/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        private static readonly Random _random = new Random();
+
+        public ActionResult Ajax()
+        {
+            var startTime = DateTime.Now;
+            //Thread.Sleep(_random.Next(5000, 10000));
+            return Json(new
+            {
+                startTime = startTime.ToString("HH:mm:ss fff"),
+                endTime = DateTime.Now.ToString("HH:mm:ss fff")
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult GetList()
+        {
+            var startTime = DateTime.Now;
+            Thread.Sleep(15 * 1000);
+            return Json(new
+            {
+                startTime = startTime.ToString("HH:mm:ss fff"),
+                endTime = DateTime.Now.ToString("HH:mm:ss fff")
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ViewInIframe()
+        {
+            return View();
+        }
+    }
+}
